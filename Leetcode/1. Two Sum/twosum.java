ffcd9aa8f977dbdce6834dfc899604cc3d4d7aa1
Sridhar G <161627732+Sridhar-GS@users.noreply.github.com>
2025-09-11 15:29:45 +0530

    Add twoSum method in Loops Brute Force

---
+++ Leetcode/1. Two Sum/twosum.java
@@ -0,0 +1,16 @@
+class Solution {
+	public int[] twoSum(int[] nums, int target) {
+        int[] twoSum = new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(i==j){
+                    break;
+                }
+                if(nums[i]+nums[j] == target){
+                    twoSum[0] = i;
+                    twoSum[1] = j;
+                }
+            }
+    	}
+	}
+}
